7e366223f0f1e798767d9ccb739fcb648424dbac
jithendrabathala <jithendrabathala@gmail.com>
2026-08-22 17:34:06 +0530

    fix(backend): update User pre-save hook for Mongoose 9 async support

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,27 +1,30 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      default: "Mahesh Chamarthy",
+    },
   },
-  password: {
-    type: String,
-    required: true
-  },
-  name: {
-    type: String,
-    default: 'Mahesh Chamarthy'
-  }
-}, { timestamps: true });
+  { timestamps: true },
+);
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
@@ -32,4 +35,4 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
