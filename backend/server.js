6d2357015e100270b7dc9c62872b17b4fbbee5fe
chamarthy.mahesh <chamarthy.mahesh@gmail.com>
2026-08-31 13:10:38 +0530

    Update CORS config for better preflight handling

--- backend/server.js
+++ backend/server.js
@@ -62,13 +62,9 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
   credentials: true
 }));
 
